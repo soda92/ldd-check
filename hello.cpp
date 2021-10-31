02b17e21b7a366b005ace52a33f439a3b4a0997e
sodacris <glorykings@foxmail.com>
2021-10-31 21:36:08 +0800

    add universal _getch()

--- hello.cpp
+++ hello.cpp
@@ -3,35 +3,34 @@
 #ifdef WIN32
 #include <conio.h>
 #else
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <termios.h> //termios, TCSANOW, ECHO, ICANON
+#include <unistd.h>	 //STDIN_FILENO
 
-int _getch(void)
+void _getch()
 {
-	struct termios oldt, newt;
-	int ch;
-	int oldf;
+	int c;
+	static struct termios oldt, newt;
 
+	/*tcgetattr gets the parameters of the current terminal
+    STDIN_FILENO will tell tcgetattr that it should write the settings
+    of stdin to oldt*/
 	tcgetattr(STDIN_FILENO, &oldt);
+	/*now the settings will be copied*/
 	newt = oldt;
+
+	/*ICANON normally takes care that one line at a time will be processed
+    that means it will return if it sees a "\n" or an EOF or an EOL*/
 	newt.c_lflag &= ~(ICANON | ECHO);
+
+	/*Those new settings will be set to STDIN
+    TCSANOW tells tcsetattr to change attributes immediately. */
 	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-	ch = getchar();
+	c = getchar();
 
+	/*restore the old settings*/
 	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-	if (ch != EOF)
-	{
-		ungetc(ch, stdin);
-		return 1;
-	}
-
-	return 0;
+	// https://stackoverflow.com/a/1798833/12291425
 }
 #endif
 
